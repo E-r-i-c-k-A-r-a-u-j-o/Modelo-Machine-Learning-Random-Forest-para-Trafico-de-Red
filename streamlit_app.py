69739d0c3d991a12f11720bbf002e96fef28b9ad
E-r-i-c-k-A-r-a-u-j-o <Statistics.earaujor@gmail.com>
2025-01-21 04:13:35 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ modelo = load("random_forest_model.pkl")
 scaler = load("random_forest_scaler.pkl")
 
 # Título de la aplicación
-st.title("Modelo de Machine Learning - Random Forest")
+st.title("GINA PRECIOSA HERMOSA este es mi Modelo de Machine Learning para predecir el Tráfico de Red")
 
 # Ingreso de datos
-st.header("Introduce los datos para predicción")
+st.header("Introduce los datos a predecir")
 features = st.text_input("Características (separadas por coma)", "1.2, 3.4, 5.6, 7.8")
 
 if st.button("Predecir"):
@@ -19,10 +19,15 @@ if st.button("Predecir"):
         input_features = np.array([float(x) for x in features.split(",")]).reshape(1, -1)
         scaled_features = scaler.transform(input_features)
         prediction = modelo.predict(scaled_features)
-        probability = modelo.predict_proba(scaled_features)
+
+        # Interpretar la predicción
+        if int(prediction[0]) == 1:
+            resultado = "El tráfico de red es normal"
+        else:
+            resultado = "El tráfico de red es un ataque"
 
         # Mostrar resultados
-        st.success(f"Predicción: {int(prediction[0])}")
-        st.info(f"Probabilidades: {probability.tolist()}")
+        st.success(f"Resultado: {resultado}")
     except Exception as e:
-        st.error(f"Error al procesar la predicción: {str(e)}")
+        st.error(f"Intenta de nuevo: {str(e)}")
+
